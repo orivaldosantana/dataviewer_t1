e6bb9256b568e4b50c560cfebf0c39cbc77477bb
Orivaldo Santana Jr <orivaldo@gmail.com>
2022-09-10 19:39:34 -0300

    up

--- utils/firebase.js
+++ utils/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfigurattion = {
-  apiKey: 'AIzaSyAHD1T5KPOZgEFlye4mxS5qN2Goo9foDP4',
-  authDomain: 'dataviewer-dev-cb866.firebaseapp.com',
-  projectId: 'dataviewer-dev-cb866',
-  storageBucket: 'dataviewer-dev-cb866.appspot.com',
-  messagingSenderId: '897416031405',
-  appId: '1:897416031405:web:4eff21823575a15ba02a24',
-  measurementId: 'G-B2X2F663DW'
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 }
 
 const app = initializeApp(firebaseConfigurattion)
