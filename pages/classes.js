d184ce46fd20bced52f3d2db8ec32628b5b7e5a9
Orivaldo Santana Jr <orivaldo@gmail.com>
2022-09-24 17:17:29 -0300

    Acessando a rota de turmas

--- pages/classes.js
+++ pages/classes.js
@@ -1,11 +1,22 @@
 import styles from '../styles/Home.module.css'
+import axios from 'axios'
 
-const Classes = () => {
+function Classes({ items }) {
+  console.log(items)
   return (
     <div className={styles.maincard}>
       <h2>Turmas</h2>
+      {items.map(c => (
+        <div key={c.id_class}> {c.name} </div>
+      ))}
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  const response = await axios.get('http://localhost:3000/api/tests/classes')
+  const items = await response.data
+  return { props: { items } }
 }
 
 export default Classes
