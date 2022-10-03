879c51f8ca0c1d000f8155e9e791db0b6976285c
Orivaldo Santana Jr <orivaldo@gmail.com>
2022-10-02 23:15:06 -0300

    ajuste css estudantes

--- components/StudentSmallCard.js
+++ components/StudentSmallCard.js
@@ -1,8 +1,12 @@
+import styles from '../styles/Student.module.css'
+
 function StudentSmallCard({ name, progress }) {
   return (
-    <div>
-      <strong> {name} </strong>
-      <p> Progresso: {progress}% </p>
+    <div className={styles.card}>
+      <div className={styles.title}>{name}</div>
+      <div className={styles.content}>
+        <p> Progresso: {progress}% </p>
+      </div>
     </div>
   )
 }
